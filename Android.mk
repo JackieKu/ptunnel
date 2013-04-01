399e5053bfd4c79dede8a9e9ed34b96ad70d75dd
Kai-Chieh Ku <kjackie@gmail.com>
2013-04-01 23:22:11 +0800

    Removed unnecessary links and use NDK to build
    It still requires libpcap, which can be found in
    AOSP. AOSP libpcap should be built fine by NDK
    without any modification.

--- Android.mk
+++ Android.mk
@@ -1,25 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := ptunnel.c md5.c
-
 LOCAL_MODULE := ptunnel
-
-LOCAL_CLFAGS += -g -O2 -Wall -Wextra -Wno-long-long -pedantic
-
-LOCAL_C_INCLUDES += external/libpcap
-
-LOCAL_STATIC_LIBRARIES := \
-	libpcap \
-	libc
-
-LOCAL_SHARED_LIBRARIES := \
-	libcutils \
-	libbinder \
-	libutils \
-	libdl
-
-LOCAL_MODULE_TAGS := eng
-
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := ptunnel.c md5.c
+LOCAL_STATIC_LIBRARIES := libpcap
 include $(BUILD_EXECUTABLE)
